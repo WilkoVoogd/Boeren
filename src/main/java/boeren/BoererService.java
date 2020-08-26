f78ad038b0f35944b008e5d900fe2b4260c989a7
WilkoVoogd <40392814+WilkoVoogd@users.noreply.github.com>
2020-08-26 19:11:18 +0200

    Created functions to collect players

--- src/main/java/boeren/BoererService.java
+++ src/main/java/boeren/BoererService.java
@@ -1,30 +1,47 @@
 package boeren;
 
-import javax.sound.midi.Soundbank;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class BoererService {
 
-    public static void main(String[] args) {
-        List<Player> players = new ArrayList<>();
+    private List<String> collectPlayersFromUserInput() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welkom in de BovenBazenBoerMachine. Hoe heet speler 1?");
-        players.add(new Player(scanner.nextLine()));
-        System.out.println("Okay leuk. En hoe heet speler 2?");
-        players.add(new Player(scanner.nextLine()));
-        System.out.println("Gekke naam. En speler 3?");
-        players.add(new Player(scanner.nextLine()));
-        System.out.println("Nu zit je gewoon random characters in te toetsen. Wie is speler 4?");
-        players.add(new Player(scanner.nextLine()));
+        String userInput = scanner.nextLine();
+        return Arrays.asList(userInput.split("\\s*,\\s*"));
+    }
 
-        Boerer boerer = new Boerer(players);
+    private List<Player> createPlayerList() {
+        System.out.println("Welkom in de BovenBazenBoerMachine");
+        System.out.println("Wie zijn de spelers? Zet een komma tussen elke speler");
+        List<String> userInputtedPlayers;
+        do {
+            userInputtedPlayers = collectPlayersFromUserInput();
+            if (userInputtedPlayers.size() != 4) {
+                System.out.println("Voer 4 makkers in");
+            }
+        } while (userInputtedPlayers.size() != 4);
+
+        List<Player> players = new ArrayList<>();
+        userInputtedPlayers.forEach(player -> players.add(new Player(player)));
+        return players;
+    }
+
+
+
+    public static void main(String[] args) throws InterruptedException {
+        BoererService service = new BoererService();
+        Boerer boerer = new Boerer(service.createPlayerList());
         boerer.appointMaten();
+        System.out.println("Oh WoW. Daar zitten een paar krokante knapen tussen");
+        System.out.println("een momentje...");
         Maten teamOne = boerer.getMatenList().get(0);
         Maten teamTwo = boerer.getMatenList().get(1);
-
-        System.out.println("okay. De maten deze boom zijn: "
+        Thread.sleep(1500);
+        System.out.println();
+        System.out.println("okay. De Malse Maten deze boom zijn: "
                 + System.lineSeparator()
                 + " -- "
                 + teamOne.getPlayerOne().getName()
@@ -35,5 +52,7 @@ public class BoererService {
                 + teamTwo.getPlayerOne().getName()
                 + " & "
                 + teamTwo.getPlayerTwo().getName());
+
+        Thread.sleep(99999);
     }
 }
